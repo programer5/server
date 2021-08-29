9502942f83e1a18bc9d3e6a5775ae536748180b5
minseo23 <neverdie4757@gmail.com>
2021-08-30 01:10:19 +0900

    server api

---
+++ src/main/java/com/example/server/controller/ServerApiController.java
@@ -0,0 +1,34 @@
+package com.example.server.controller;
+
+import com.example.server.dto.Req;
+import com.example.server.dto.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/server")
+public class ServerApiController {
+
+    @GetMapping("/hello")
+    public User hello(@RequestParam String name, @RequestParam int age) {
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+        return user;
+    }
+
+    @PostMapping("/user/{userId}/name/{userName}")
+    public Req<User> post(@RequestBody Req<User> user, @PathVariable int userId, @PathVariable String userName,
+                          @RequestHeader("x-authorization") String authorization,
+                          @RequestHeader("custom-header") String customHeader) {
+      log.info("client request = {}", user);
+      log.info("userId = {}, userName = {}", userId, userName);
+      log.info("authorization = {}, customHeader = {}", authorization, customHeader);
+
+      Req<User> response = new Req<>();
+      response.setHeader(new Req.Header());
+      response.setBody(user.getBody());
+      return response;
+    }
+}
