916083afe0eeb41198dfa29552678469d94d3351
minseo23 <neverdie4757@gmail.com>
2021-08-30 22:22:51 +0900

    Rest Template 사용해서 naver 지역 검색 API 연동해 보기

--- src/main/java/com/example/server/controller/ServerApiController.java
+++ src/main/java/com/example/server/controller/ServerApiController.java
@@ -3,12 +3,40 @@ package com.example.server.controller;
 import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    @GetMapping("/naver")
+    public String naver() {
+        URI uri = UriComponentsBuilder.fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", "중국집")
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode()
+                .build()
+                .toUri();
+
+
+        RestTemplate restTemplate = new RestTemplate();
+        RequestEntity<Void> req = RequestEntity.get(uri).header("X-Naver-Client-Id", "v5dJzgkTn_sABo2hCdHo")
+                .header("X-Naver-Client-Secret", "zA4J6k0G7o").build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
+    }
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age) {
